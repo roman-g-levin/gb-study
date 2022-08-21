4eaa75fb3031555441ee4dac37fcdcb5a3fc566a
Roman Levin <roman_levin@mail.ru>
2022-08-21 17:12:35 +0500

    решение задачи 8

---
+++ c#/seminar1/task8/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Программа вывода всех четных числе от 1 до N");
+
+Console.Write("Введите число:");
+int iNum = int.Parse(Console.ReadLine());
+int iCurrent = 2;
+
+while(iCurrent<=iNum){
+    Console.Write($"{iCurrent} ");
+    iCurrent = iCurrent + 2;
+}
