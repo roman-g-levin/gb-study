4a158ecbba1fc3602065004eb5f7152468359314
Roman Levin <roman_levin@mail.ru>
2022-08-21 17:05:26 +0500

    решение задачи 6

---
+++ c#/seminar1/task6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Программа определения четности числа");
+
+Console.Write("Введите число:");
+int iNum = int.Parse(Console.ReadLine());
+int iDiv2 = iNum / 2;
+
+if (iNum == iDiv2*2){
+    Console.WriteLine("Введено четное число");
+}
+else{
+    Console.WriteLine("Введено нечетное число");
+}
