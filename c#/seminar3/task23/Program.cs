a69ddef20545b312702aa2ee15e59361780d80c1
Roman Levin <roman_levin@mail.ru>
2022-08-31 13:10:24 +0500

    программа вывода таблицы кубов от 1 до N

---
+++ c#/seminar3/task23/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 23
+Напишите программу, которая принимает на вход число (N)
+и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+Console.WriteLine("Программа вывода кубов чисел от 1 до N");
+
+//ввод данных
+Console.Write("Введите целое положительное число N:");
+int iN = int.Parse(Console.ReadLine());
+
+if (iN > 0)
+{
+    Console.Write($"{iN} -> 1");
+    for (int iCount = 2; iCount <= iN; iCount++) Console.Write($", {Math.Pow(iCount, 3)}");
+}
+else
+    Console.WriteLine("Введено неверное число");
