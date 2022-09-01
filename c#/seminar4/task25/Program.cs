02355a3cb32693a5fc7971a8e200a4a3f97ac93c
Roman Levin <roman_levin@mail.ru>
2022-09-01 12:57:16 +0500

    программа возведения числа А а степень В

---
+++ c#/seminar4/task25/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+//объявление функций и методов
+int XpowY(int iX, int iY)
+{
+    int iRes=iX;
+    for(int i=1;i<iY;i++) iRes*=iX;
+    return iRes;
+}
+//начало программы
+Console.WriteLine("Программа возведения числа А в степень В");
+
+//ввод данных
+Console.Write("Введите целое число А:");
+int iA = int.Parse(Console.ReadLine());
+Console.Write("Введите натуральное число В:");
+int iB = int.Parse(Console.ReadLine());
+
+//проверка на корректность введенных данных
+if (iB<=0) Console.WriteLine("Число В не натуральное!");
+else Console.WriteLine($"({iA})^{iB} = {XpowY(iA,iB)}");     //вывод результата
