2f92568ea45aaa6864cf148b5e81781cf8db46a4
Roman Levin <roman_levin@mail.ru>
2022-09-18 16:44:35 +0500

    Программа суммирования наруральных элементов от M до N

---
+++ c#/seminar9/task66/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Задача 66:
+Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов
+в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+// Объявление методов и функций
+int SummFromMtoN(int m, int n)
+{
+    int summ = 0;
+    for (int i = m; i <= n; i++)
+        summ += i;
+    return summ;
+}
+// Начало программы
+Console.WriteLine("Программа поиска суммы натуральных элементов от M до N");
+
+// Ввод данных
+Console.Write("Введите натуральное число M:");
+int iM = int.Parse(Console.ReadLine());
+Console.Write("Введите натуральное число N:");
+int iN = int.Parse(Console.ReadLine());
+
+// Проверка корректности ввода
+if (iM <= 0 || iN <= 0) Console.WriteLine("Некорректный ввод");
+else
+{
+    // Подсчет и вывод суммы элементов
+    Console.WriteLine($"Сумма натуральных элементов от {iM} до {iN} = {SummFromMtoN(iM, iN)}");
+}
