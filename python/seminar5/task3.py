bf5a023f1289abba7e2b0e92ecb99566dbfdf289
Roman Levin <roman_levin@mail.ru>
2022-11-06 02:04:10 +0500

    Решение задач семинара 5

---
+++ python/seminar5/task3.py
@@ -0,0 +1,66 @@
+﻿"""
+Создайте программу для игры в ""Крестики-нолики"".
+"""
+
+# объявление функций и методов
+
+
+def display(a):
+    print(f' {a[1]} | {a[2]} | {a[3]}      1 | 2 | 3')
+    print('---+---+---    ---+---+---')
+    print(f' {a[4]} | {a[5]} | {a[6]}      4 | 5 | 6')
+    print('---+---+---    ---+---+---')
+    print(f' {a[7]} | {a[8]} | {a[9]}      7 | 8 | 9')
+
+
+def win(a):
+    if a[1] == a[2] and a[2] == a[3] and a[3] != ' ':
+        return a[3]
+    elif a[4] == a[5] and a[5] == a[6] and a[6] != ' ':
+        return a[6]
+    elif a[7] == a[8] and a[8] == a[9] and a[9] != ' ':
+        return a[9]
+    elif a[1] == a[4] and a[4] == a[7] and a[7] != ' ':
+        return a[7]
+    elif a[2] == a[5] and a[5] == a[8] and a[8] != ' ':
+        return a[8]
+    elif a[3] == a[6] and a[6] == a[9] and a[9] != ' ':
+        return a[9]
+    elif a[1] == a[5] and a[5] == a[9] and a[9] != ' ':
+        return a[9]
+    elif a[3] == a[5] and a[5] == a[7] and a[7] != ' ':
+        return a[7]
+    else:
+        return ' '
+
+
+# начало программы
+print('Программа игры в крестики-нолики. Нумерация полей:')
+print(' 1 | 2 | 3')
+print('---+---+---')
+print(' 4 | 5 | 6')
+print('---+---+---')
+print(' 7 | 8 | 9\n')
+
+array = [" " for i in range(10)]
+player = 'X'
+
+while win(array) == ' ':
+    display(array)
+    print(f'Ход игрока {player}')
+    hod = int(input(f'Введите номер поля:'))
+    if hod < 1 or hod > 9:
+        print('Неверный номер поля')
+        continue
+    if array[hod] != ' ':
+        print('Поле занято, повторите ввод')
+        continue
+    else:
+        array[hod] = player
+    if player == 'X':
+        player = 'O'
+    else:
+        player = 'X'
+
+display(array)
+print(f'Выиграл игрок {win(array)}')
---
+++ python/seminar5/task4.py
@@ -0,0 +1,44 @@
+﻿"""
+Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+Входные и выходные данные хранятся в отдельных текстовых файлах.
+"""
+
+# объявление функций и методов
+def compress(s):
+    if len(s)==0:
+        return ''
+    ch = s[0]
+    i = 1
+    if len(s) < 10:
+        lim = len(s)
+    else:
+        lim = 10
+    for i in range(1, lim):
+        if s[i] != ch:
+            break
+    return ch+str(i)+compress(s[i:])
+
+def decompress(s):
+    if len(s)==0:
+        return ''
+    return s[0]*int(s[1])+decompress(s[2:])
+
+# начало программы
+print('Программа реализации RLE-алгоритма')
+print('Сжатие:')
+path_in = 'task4_in.txt'
+path_out = 'task4_out.txt'
+data = open(path_in, 'r')
+dataout = open(path_out, 'w')
+for line in data:
+    print(f'{line.rstrip()} -> {compress(line.rstrip())}')
+    dataout.write(compress(line.rstrip())+'\n')
+data.close()
+dataout.close()
+
+print('Распаковка:')
+path_in = 'task4_out.txt'
+data = open(path_in, 'r')
+for line in data:
+    print(f'{line.rstrip()} -> {decompress(line.rstrip())}')
+data.close()
