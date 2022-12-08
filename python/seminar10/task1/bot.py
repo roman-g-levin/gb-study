7b26e5644e45926de70ebe4d9334ed652190efaf
Roman Levin <roman_levin@mail.ru>
2022-12-08 20:31:01 +0500

    Исправление ошибок и добавление операций умножения и деления

--- python/seminar10/task1/bot.py
+++ python/seminar10/task1/bot.py
@@ -1,24 +1,25 @@
 import logging
-
+from datetime import datetime
 from telegram.ext import Updater, CommandHandler, ConversationHandler, MessageHandler, Filters, CallbackQueryHandler
-
-# Enable logging
 from commands import start_command
-from handlers import OP_BUTTON_STATE, op_select_handler, op_input_handler, OP_INPUT_STATE, NUM_A_STATE, NUM_B_STATE, \
+from handlers import OP_BUTTON_STATE, op_select_handler,\
+    op_input_handler, OP_INPUT_STATE, NUM_A_STATE, NUM_B_STATE, \
     num_b_handler, num_a_handler
-
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-)
-logger = logging.getLogger(__name__)
 
 #TOKEN = ""
 from my_bot_token import TOKEN
 
 
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
 
 def main() -> None:
     """Start the bot."""
+    logger.info("Запуск бота {}".format(datetime.now().time()))
 
     # Create the Updater and pass it your bot's token.
     updater = Updater(TOKEN)
@@ -35,7 +36,7 @@ def main() -> None:
             OP_BUTTON_STATE: [MessageHandler(Filters.regex(r"R|C"), op_select_handler)],
             OP_INPUT_STATE: [CallbackQueryHandler(op_input_handler)],
             NUM_A_STATE: [MessageHandler(Filters.text, num_a_handler)],
-            NUM_B_STATE: [MessageHandler(Filters.text, num_b_handler)],
+            NUM_B_STATE: [MessageHandler(Filters.text, num_b_handler)]
         },
         fallbacks=[]
     )
@@ -44,11 +45,13 @@ def main() -> None:
 
     # Start the Bot
     updater.start_polling()
+    logger.info("Бот запущен {}".format(datetime.now().time()))
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
+    logger.info("Бот остановлен {}".format(datetime.now().time()))
 
 
 if __name__ == '__main__':
--- python/seminar10/task1/handlers.py
+++ python/seminar10/task1/handlers.py
@@ -3,14 +3,15 @@ import pickle
 
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import CallbackContext, ConversationHandler
+#from bot import logger
+#from datetime import datetime
 
 (
-    OP_BUTTON_STATE,  # 0
+    OP_BUTTON_STATE,
     OP_INPUT_STATE,
-    NUM_A_STATE,      # 1 ...
-    NUM_B_STATE,
+    NUM_A_STATE,
+    NUM_B_STATE
 ) = range(4)  # [0, 1, 2, ..]
-
 
 
 def save_data(data, chat_id):
@@ -24,7 +25,7 @@ def load_data(chat_id):
         return None
 
     with open(file_name, 'rb') as f:
-        pickle.load(f)
+        return pickle.load(f)
 
 
 def op_select_handler(update: Update, context: CallbackContext) -> int:
@@ -42,20 +43,21 @@ def op_select_handler(update: Update, context: CallbackContext) -> int:
         }
 
 
-
     if "R" in text:
         data["num_type"] = float
     elif "C" in text:
         data["num_type"] = complex
 
     save_data(data, update.effective_user.id)
+    #logger.info("{} Пользователь {} выбрал работу с {}".format(datetime.now().time(), update.effective_user.id, data["num_type"]))
 
     # Предложить пользователию выбрать действие InlineKeyboard
     kb = [
-        [InlineKeyboardButton("a + b", callback_data="+"), InlineKeyboardButton("a - b", callback_data="-")],
+        [InlineKeyboardButton("A + B", callback_data="+"), InlineKeyboardButton("A - B", callback_data="-")],
+        [InlineKeyboardButton("A * B", callback_data="*"), InlineKeyboardButton("A / B", callback_data="/")]
     ]
     reply_kb_markup = InlineKeyboardMarkup(kb)
-    update.message.reply_text(f"{data} \nВыберите операцию: ", reply_markup=reply_kb_markup)
+    update.message.reply_text(f"{data['username']},\nВыберите операцию: ", reply_markup=reply_kb_markup)
     return OP_INPUT_STATE
 
 
@@ -66,7 +68,8 @@ def op_input_handler(update: Update, context: CallbackContext) -> int:
     data["op"] = update.callback_query.data   # +, -
 
     save_data(data, update.effective_user.id)
-    update.callback_query.message.edit_text("Введите число А: ")
+    #logger.info("{} Пользователь {} выбрал работу с {}".format(datetime.now().time(), update.effective_user.id, data["op"]))
+    update.callback_query.message.edit_text("Введите число A: ")
     return NUM_A_STATE
 
 def num_a_handler(update: Update, context: CallbackContext) -> int:
@@ -75,8 +78,9 @@ def num_a_handler(update: Update, context: CallbackContext) -> int:
     data["num_a"] = update.message.text
 
     save_data(data, update.effective_user.id)
+    #logger.info("{} Пользователь {} ввел число A {}".format(datetime.now().time(), update.effective_user.id, data["num_a"]))
 
-    update.message.reply_text("Введите число Б: ")
+    update.message.reply_text("Введите число B: ")
     return NUM_B_STATE
 
 
@@ -87,10 +91,14 @@ def num_b_handler(update: Update, context: CallbackContext) -> int:
     # выполнить рассчет и вывести ответ
     num_a = data["num_type"](data["num_a"])     # float(data["num_a"]) -> float('2')
     num_b = data["num_type"](data["num_b"])
-    data["result"] = eval(f'{num_a} {data["op"]} {num_b}')
+    if data["op"]=='/' and (num_b==0 or num_b==complex(0j)):
+        data["result"] = "деление на 0 невозможно"
+    else:
+        data["result"] = eval(f'{num_a} {data["op"]} {num_b}')
 
     save_data(data, update.effective_user.id)
-
-    update.message.reply_text(f"Результат вычислений {data['result']}")
+    #logger.info("{} Пользователь {} ввел число B {}".format(datetime.now().time(), update.effective_user.id, data["num_b"]))
+    #logger.info("{} Пользователь {} Результат вычислений {}".format(datetime.now().time(), update.effective_user.id, data["result"]))
+    update.message.reply_text(f"{data['username']},\nРезультат вычислений: {data['result']}")
 
     return ConversationHandler.END
