87edf6897ac9d380abf83614cc593b3ee9846e36
Roman Levin <roman_levin@mail.ru>
2023-05-15 00:55:10 +0500

    Решение практического задания Андроид 2

--- android/project2/app/src/main/java/com/example/project2/MainActivity.java
+++ android/project2/app/src/main/java/com/example/project2/MainActivity.java
@@ -3,38 +3,50 @@ package com.example.project2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+private static final String LOGID = "myLogs";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(LOGID,"onCreate called");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Toast.makeText(this,"", Toast.LENGTH_LONG).show();
     }
 
     @Override
     protected void onStop() {
+        Log.d(LOGID,"onStop called");
         super.onStop();
     }
 
     @Override
     protected void onRestart() {
+        Log.d(LOGID,"onRestart called");
         super.onRestart();
     }
 
     @Override
     protected void onPause() {
+        Log.d(LOGID,"onPause called");
         super.onPause();
     }
 
     @Override
     protected void onResume() {
+        Log.d(LOGID,"onResume called");
         super.onResume();
     }
 
     @Override
     protected void onStart() {
+        Log.d(LOGID,"onStart called");
         super.onStart();
     }
 
