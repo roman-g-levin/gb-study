9a1d251c264fcc7c1ab895d8f334731dd6b01c28
Roman Levin <roman_levin@mail.ru>
2024-02-20 12:06:44 +0500

    Решение задач семинара 6

---
+++ ai/seminar6/tg_bot.py
@@ -0,0 +1,64 @@
+import telebot
+from random import *
+import json
+import requests
+from my_bot_token import API_TOKEN
+
+films=[]
+API_URL='https://7012.deeppavlov.ai/model'
+
+def save():
+    with open("films.json","w",encoding="utf-8") as fh:
+        fh.write(json.dumps(films,ensure_ascii=False))
+    print("Наша фильмотека была успешно сохранена в файле films.json")
+
+def load():
+    global films
+    with open("films.json","r",encoding="utf-8") as fh:
+        films=json.load(fh)
+    print("Фильмотека была успешно загружена")   
+
+
+bot = telebot.TeleBot(API_TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    try:
+        load()
+        bot.send_message(message.chat.id,"Фильмотека была успешно загружена!")
+
+    except:
+        films.append("Матрица")
+        films.append("Солярис")
+        films.append("Властелин колец")
+        films.append("Техасская резня бензопилой")
+        films.append("Санта Барбара") 
+        bot.send_message(message.chat.id,"Фильмотека была загружена по умолчанию!")
+
+@bot.message_handler(commands=['save'])
+def save_films_to_file(message):
+    try:
+        save()
+        bot.send_message(message.chat.id,"Фильмотека была успешно сохранена!")
+
+    except:
+        bot.send_message(message.chat.id,"Ошибка при сохранении фильмотеки!")
+
+@bot.message_handler(commands=['all'])
+def show_all(message):
+    bot.send_message(message.chat.id,"Вот список фильмов")
+    bot.send_message(message.chat.id, "\n".join(films))
+
+@bot.message_handler(commands=['wiki'])
+def wiki(message):
+    quest = message.text.split()[1:]
+    qq=" ".join(quest)
+    data = { 'question_raw': [qq]}
+    #print(quest, qq, data)
+    try:
+        res = requests.post(API_URL,json=data,verify=False).json()
+        bot.send_message(message.chat.id, res)
+    except:
+        bot.send_message(message.chat.id, "Что-то я ничего не нашел :-(")
+
+bot.polling()
